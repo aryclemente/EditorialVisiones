2016f3aad3aac81e6ec9270d82c5cc84facd69d8
aryclemente <hello.aryc@gmail.com>
2023-04-09 23:01:37 -0400

    Base de datos con los cambios anteriormente mencionados

--- wp-content/themes/skt-fse/includes/patterns/skt-fse-lista-programas.php
+++ wp-content/themes/skt-fse/includes/patterns/skt-fse-lista-programas.php
@@ -3,54 +3,24 @@
  * Programas
  */
 return array(
-	'title'      => __( 'Programas', 'skt-fse' ),
+	'title'      => __( 'Lista-Programas', 'skt-fse' ),
 	'categories' => array( 'skt-fse' ),
 	'blockTypes' => array( 'core/template-part/skt-fse' ),
 	'content'    => '<!-- wp:group {"tagName":"main"} -->
     <main class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"80px","left":"15px","right":"15px"}}},"backgroundColor":"background","className":"skt-fse-blog-single-area","layout":{"type":"constrained","contentSize":"1200px"}} -->
-    <div class="wp-block-group skt-fse-blog-single-area has-background-background-color has-background" style="padding-top:80px;padding-right:15px;padding-bottom:80px;padding-left:15px"><!-- wp:group {"tagName":"section","style":{"color":{"background":"#eff5fa"},"spacing":{"padding":{"bottom":"80px"}}},"className":"skt-fse-sec skt-fse-section7","layout":{"type":"constrained","contentSize":"1230px"}} -->
-    <section class="wp-block-group skt-fse-sec skt-fse-section7 has-background" style="background-color:#eff5fa;padding-bottom:80px"><!-- wp:columns {"style":{"spacing":{"padding":{"bottom":"25px"}}}} -->
-    <div class="wp-block-columns" style="padding-bottom:25px"><!-- wp:column {"width":"25%"} -->
-    <div class="wp-block-column" style="flex-basis:25%"></div>
-    <!-- /wp:column -->
+    <div class="wp-block-group skt-fse-blog-single-area has-background-background-color has-background" style="padding-top:80px;padding-right:15px;padding-bottom:80px;padding-left:15px"><!-- wp:query {"queryId":32,"query":{"perPage":6,"pages":0,"offset":0,"postType":"programas","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]},"displayLayout":{"type":"flex","columns":3},"layout":{"type":"constrained","contentSize":""}} -->
+    <div class="wp-block-query"><!-- wp:post-template {"backgroundColor":"background"} -->
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"50px","right":"35px","bottom":"50px","left":"35px"}},"color":{"background":"#eff5fa"},"border":{"radius":"20px"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group has-background" style="border-radius:20px;background-color:#eff5fa;padding-top:50px;padding-right:35px;padding-bottom:50px;padding-left:35px"><!-- wp:post-featured-image {"isLink":true,"width":"200px","height":"200px","dimRatio":10,"align":"center","style":{"color":{"duotone":"unset"},"border":{"radius":"45px","width":"0px","style":"none"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} /-->
     
-    <!-- wp:column {"width":"50%"} -->
-    <div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading {"textAlign":"center","level":5,"style":{"color":{"text":"#6458ef"},"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","textTransform":"capitalize"},"spacing":{"padding":{"top":"0px","bottom":"0px"}}},"fontFamily":"poppins"} -->
-    <h5 class="wp-block-heading has-text-align-center has-text-color has-poppins-font-family" style="color:#6458ef;padding-top:0px;padding-bottom:0px;font-size:18px;font-style:normal;font-weight:600;text-transform:capitalize">Educación Digital</h5>
-    <!-- /wp:heading -->
+    <!-- wp:post-title {"textAlign":"center","level":1,"isLink":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","textTransform":"capitalize"},"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"fontSize":"large"} /-->
     
-    <!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#0a1d2d"},"typography":{"fontSize":"40px","fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"15px","bottom":"30px"}}},"fontFamily":"poppins"} -->
-    <h2 class="wp-block-heading has-text-align-center has-text-color has-poppins-font-family" style="color:#0a1d2d;padding-top:15px;padding-bottom:30px;font-size:40px;font-style:normal;font-weight:700">Programas de Formación</h2>
-    <!-- /wp:heading --></div>
-    <!-- /wp:column -->
+    <!-- wp:post-excerpt {"fontSize":"medium"} /-->
     
-    <!-- wp:column {"width":"25%"} -->
-    <div class="wp-block-column" style="flex-basis:25%"></div>
-    <!-- /wp:column --></div>
-    <!-- /wp:columns -->
-    
-    <!-- wp:query {"queryId":15,"query":{"perPage":3,"pages":0,"offset":0,"postType":"programas","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]},"displayLayout":{"type":"flex","columns":3},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-query"><!-- wp:post-title /-->
-    
-    <!-- wp:post-featured-image /-->
-    
-    <!-- wp:post-date /-->
-    
-    <!-- wp:query-pagination -->
-    <!-- wp:query-pagination-previous /-->
-    
-    <!-- wp:query-pagination-numbers /-->
-    
-    <!-- wp:query-pagination-next /-->
-    <!-- /wp:query-pagination -->
-    
-    <!-- wp:query-no-results -->
-    <!-- wp:paragraph {"placeholder":"Añade texto o bloques que se mostrarán cuando la consulta no devuelva ningún resultado."} -->
-    <p></p>
-    <!-- /wp:paragraph -->
-    <!-- /wp:query-no-results --></div>
-    <!-- /wp:query --></section>
-    <!-- /wp:group --></div>
+    <!-- wp:post-date {"textAlign":"center"} /--></div>
+    <!-- /wp:group -->
+    <!-- /wp:post-template --></div>
+    <!-- /wp:query --></div>
     <!-- /wp:group --></main>
     <!-- /wp:group -->',
 );
